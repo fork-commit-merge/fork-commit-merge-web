6db8834b4c9f9d8796ec272212ef6ccd03bb6b51
Niko Hoffrén <niko.hoffren@gmail.com>
2023-07-26 13:30:09 +0300

    Update website name to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,26 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen flex flex-col items-center justify-center p-10 text-center bg-slate-950">
-      <div className="mb-10">
-        <Image
-          className="mx-auto"
-          src="/dev-gallery.jpg"
-          alt="Dev Gallery Logo"
-          width={180}
-          height={180}
-          priority
-        />
-        <h1 className="mt-5 text-5xl font-bold text-slate-50">Welcome to Dev Gallery</h1>
-        <p className="mt-2 text-lg font-light text-gray-100">Explore amazing projects developed by talented developers</p>
-      </div>
+    return (
+        <main className="min-h-screen flex flex-col items-center justify-center p-10 text-center bg-slate-950">
+            <div className="mb-10">
+                <Image
+                    className="mx-auto"
+                    src="/dev-gallery.jpg"
+                    alt="Dev Gallery Logo"
+                    width={180}
+                    height={180}
+                    priority
+                />
+                <h1 className="mt-5 text-5xl font-bold text-slate-50">
+                    Welcome to devgallery.io
+                </h1>
+                <p className="mt-2 text-lg font-light text-gray-100">
+                    Explore amazing projects developed by talented developers
+                </p>
+            </div>
 
-      {/* <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {/* <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {projects.map((project) => (
           <div key={project.id} className="p-4 border rounded-lg shadow-sm">
             <h2 className="mb-2 text-2xl font-semibold">{project.name}</h2>
@@ -24,7 +28,6 @@ export default function Home() {
           </div>
         ))}
       </div> */}
-    </main>
-  )
+        </main>
+    );
 }
-
