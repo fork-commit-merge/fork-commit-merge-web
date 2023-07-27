f95921783f4dc1896355384a6830fe3752a7a6cd
Luega <martinelliluca90@gmail.com>
2023-07-27 20:00:09 +0200

    ISSUE:Added Login and SignIn links in header in components/Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,210 +8,220 @@ import styles from "../styles/Coffee.module.css";
 import React from "react";
 
 const Header = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [isDropdownOpen, setDropdownOpen] = useState(false);
-    const node = React.useRef<HTMLDivElement | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const node = React.useRef<HTMLDivElement | null>(null);
 
-    const handleClickOutside = (e: MouseEvent) => {
-        if (node.current?.contains(e.target as Node)) {
-            return;
-        }
-        setDropdownOpen(false);
+  const handleClickOutside = (e: MouseEvent) => {
+    if (node.current?.contains(e.target as Node)) {
+      return;
+    }
+    setDropdownOpen(false);
+  };
+
+  useEffect(() => {
+    if (isDropdownOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
     };
+  }, [isDropdownOpen]);
 
-    useEffect(() => {
-        if (isDropdownOpen) {
-            document.addEventListener("mousedown", handleClickOutside);
-        } else {
-            document.removeEventListener("mousedown", handleClickOutside);
-        }
+  const toggleSideNav = () => {
+    setIsOpen(!isOpen);
+  };
 
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, [isDropdownOpen]);
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
 
-    const toggleSideNav = () => {
-        setIsOpen(!isOpen);
-    };
-
-    const toggleDropdown = () => {
-        setDropdownOpen(!isDropdownOpen);
-    };
-
-    return (
-        <>
-            <nav className="bg-slate-950 text-slate-50 backdrop-blur">
-                <div className="container mx-auto px-6 py-3">
-                    <div className="flex items-center justify-between">
-                        <div>
-                            <Link href="/">
-                                <div className="py-2 px-6 block text-sm cursor-pointer hover:scale-105">
-                                    <Image
-                                        src="/dev-gallery.png"
-                                        alt="devgallery.io logo"
-                                        width={40}
-                                        height={40}
-                                    />
-                                </div>
-                            </Link>
-                        </div>
-                        <div className="md:hidden">
-                            <button onClick={toggleSideNav}>
-                                {isOpen ? (
-                                    <XIcon className="h-6 w-6" />
-                                ) : (
-                                    <MenuIcon className="h-6 w-6" />
-                                )}
-                            </button>
-                        </div>
-                        <nav className="hidden md:block">
-                            <ul className="md:flex items-center space-x-1">
-                                <li>
-                                    <Link href="/getting-started">
-                                        <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
-                                            Getting Started
-                                        </div>
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link
-                                        href="https://github.com/nikohoffren/devgallery.io"
-                                        target="blank"
-                                    >
-                                        <div className="py-2 px-6 mr-6 block text-sm cursor-pointer hover:underline">
-                                            Contribute in GitHub
-                                        </div>
-                                    </Link>
-                                </li>
-                                <li className={styles.buyButton}>
-                                    <Coffee />
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
+  return (
+    <>
+      <nav className="bg-slate-950 text-slate-50 backdrop-blur">
+        <div className="container mx-auto px-6 py-3">
+          <div className="flex items-center justify-between">
+            <div>
+              <Link href="/">
+                <div className="py-2 px-6 block text-sm cursor-pointer hover:scale-105">
+                  <Image
+                    src="/dev-gallery.png"
+                    alt="devgallery.io logo"
+                    width={40}
+                    height={40}
+                  />
                 </div>
+              </Link>
+            </div>
+            <div className="md:hidden">
+              <button onClick={toggleSideNav}>
+                {isOpen ? (
+                  <XIcon className="h-6 w-6" />
+                ) : (
+                  <MenuIcon className="h-6 w-6" />
+                )}
+              </button>
+            </div>
+            <nav className="hidden md:block">
+              <ul className="md:flex items-center space-x-1">
+                <li>
+                  <Link href="/getting-started">
+                    <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
+                      Getting Started
+                    </div>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://github.com/nikohoffren/devgallery.io"
+                    target="blank"
+                  >
+                    <div className="py-2 px-6 mr-6 block text-sm cursor-pointer hover:underline">
+                      Contribute in GitHub
+                    </div>
+                  </Link>
+                </li>
+                <li className={styles.buyButton}>
+                  <Coffee />
+                </li>
+              </ul>
             </nav>
-            {isOpen && (
-                <div className="fixed top-0 left-0 w-64 h-full pt-6 text-slate-50 bg-slate-900 overflow-auto z-20 md:hidden">
-                    <div className="ml-6 mb-6">
-                        <div className={styles.buyButton}>
-                            <Coffee />
-                        </div>
-                    </div>
-                    <Link href="/getting-started">
-                        <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
-                            Getting Started
-                        </div>
-                    </Link>
-                    <Link
-                        href="https://github.com/nikohoffren/devgallery.io"
-                        target="blank"
-                    >
-                        <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
-                            Contribute in GitHub
-                        </div>
-                    </Link>
-                    <div className="relative" ref={node}>
-                        <button
-                            onClick={toggleDropdown}
-                            className="py-2 px-6 block text-sm cursor-pointer hover:underline"
-                        >
-                            Projects
-                        </button>
-                        {isDropdownOpen && (
-                            <div className="absolute left-0 w-48 py-2 mt-2 bg-slate-900 rounded-lg shadow-xl">
-                                <Link
-                                    href="/projects/websites"
-                                    className="block px-4 py-2 text-sm cursor-pointer hover:underline"
-                                >
-                                    Websites
-                                </Link>
-                                <Link
-                                    href="/projects/mobile-apps"
-                                    className="block px-4 py-2 text-sm cursor-pointer hover:underline"
-                                >
-                                    Mobile Apps
-                                </Link>
-                                <Link
-                                    href="/projects/desktop-apps"
-                                    className="block px-4 py-2 text-sm cursor-pointer hover:underline"
-                                >
-                                    Desktop Apps
-                                </Link>
-                            </div>
-                        )}
-                    </div>
-                    <Link href="/developers">
-                        <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
-                            Developers
-                        </div>
-                    </Link>
-                    <Link href="/about">
-                        <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
-                            About
-                        </div>
-                    </Link>
-                    <Link href="/contact">
-                        <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
-                            Contact
-                        </div>
-                    </Link>
-                </div>
+          </div>
+        </div>
+      </nav>
+      {isOpen && (
+        <div className="fixed top-0 left-0 w-64 h-full pt-6 text-slate-50 bg-slate-900 overflow-auto z-20 md:hidden">
+          <div className="ml-6 mb-6">
+            <div className={styles.buyButton}>
+              <Coffee />
+            </div>
+          </div>
+          <Link href="/getting-started">
+            <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
+              Getting Started
+            </div>
+          </Link>
+          <Link
+            href="https://github.com/nikohoffren/devgallery.io"
+            target="blank"
+          >
+            <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
+              Contribute in GitHub
+            </div>
+          </Link>
+          <div className="relative" ref={node}>
+            <button
+              onClick={toggleDropdown}
+              className="py-2 px-6 block text-sm cursor-pointer hover:underline"
+            >
+              Projects
+            </button>
+            {isDropdownOpen && (
+              <div className="absolute left-0 w-48 py-2 mt-2 bg-slate-900 rounded-lg shadow-xl">
+                <Link
+                  href="/projects/websites"
+                  className="block px-4 py-2 text-sm cursor-pointer hover:underline"
+                >
+                  Websites
+                </Link>
+                <Link
+                  href="/projects/mobile-apps"
+                  className="block px-4 py-2 text-sm cursor-pointer hover:underline"
+                >
+                  Mobile Apps
+                </Link>
+                <Link
+                  href="/projects/desktop-apps"
+                  className="block px-4 py-2 text-sm cursor-pointer hover:underline"
+                >
+                  Desktop Apps
+                </Link>
+              </div>
             )}
-            <header className="bg-slate-950 text-slate-50 sticky top-0 z-10 hidden md:block">
-                <div className="container mx-auto px-6 py-1 pb-6">
-                    <div className="mt-3 md:flex items-center space-x-1 justify-end">
-                        <div className="relative" ref={node}>
-                            <button
-                                onClick={toggleDropdown}
-                                className="py-2 px-6 block text-sm cursor-pointer hover:underline"
-                            >
-                                Projects
-                            </button>
-                            {isDropdownOpen && (
-                                <div className="absolute left-0 w-48 py-2 mt-2 bg-slate-900 rounded-lg shadow-xl">
-                                    <Link
-                                        href="/projects/websites"
-                                        className="block px-4 py-2 text-sm cursor-pointer hover:underline"
-                                    >
-                                        Websites
-                                    </Link>
-                                    <Link
-                                        href="/projects/mobile-apps"
-                                        className="block px-4 py-2 text-sm cursor-pointer hover:underline"
-                                    >
-                                        Mobile Apps
-                                    </Link>
-                                    <Link
-                                        href="/projects/desktop-apps"
-                                        className="block px-4 py-2 text-sm cursor-pointer hover:underline"
-                                    >
-                                        Desktop Apps
-                                    </Link>
-                                </div>
-                            )}
-                        </div>
-                        <Link href="/developers">
-                            <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
-                                Developers
-                            </div>
-                        </Link>
-                        <Link href="/about">
-                            <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
-                                About
-                            </div>
-                        </Link>
-                        <Link href="/contact">
-                            <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
-                                Contact
-                            </div>
-                        </Link>
-                    </div>
+          </div>
+          <Link href="/developers">
+            <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
+              Developers
+            </div>
+          </Link>
+          <Link href="/about">
+            <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
+              About
+            </div>
+          </Link>
+          <Link href="/contact">
+            <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
+              Contact
+            </div>
+          </Link>
+        </div>
+      )}
+      <header className="bg-slate-950 text-slate-50 sticky top-0 z-10 hidden md:block">
+        <div className="container mx-auto px-6 py-1 pb-6">
+          <div className="mt-3 md:flex items-center space-x-1 justify-end">
+            <div className="relative" ref={node}>
+              <button
+                onClick={toggleDropdown}
+                className="py-2 px-6 block text-sm cursor-pointer hover:underline"
+              >
+                Projects
+              </button>
+              {isDropdownOpen && (
+                <div className="absolute left-0 w-48 py-2 mt-2 bg-slate-900 rounded-lg shadow-xl">
+                  <Link
+                    href="/projects/websites"
+                    className="block px-4 py-2 text-sm cursor-pointer hover:underline"
+                  >
+                    Websites
+                  </Link>
+                  <Link
+                    href="/projects/mobile-apps"
+                    className="block px-4 py-2 text-sm cursor-pointer hover:underline"
+                  >
+                    Mobile Apps
+                  </Link>
+                  <Link
+                    href="/projects/desktop-apps"
+                    className="block px-4 py-2 text-sm cursor-pointer hover:underline"
+                  >
+                    Desktop Apps
+                  </Link>
                 </div>
-            </header>
-        </>
-    );
+              )}
+            </div>
+            <Link href="/developers">
+              <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
+                Developers
+              </div>
+            </Link>
+            <Link href="/about">
+              <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
+                About
+              </div>
+            </Link>
+            <Link href="/contact">
+              <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
+                Contact
+              </div>
+            </Link>
+            <Link href="/login">
+              <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
+                Login
+              </div>
+            </Link>
+            <Link href="/sign-up">
+              <div className="py-2 px-6 block text-sm cursor-pointer hover:underline">
+                Sign up
+              </div>
+            </Link>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
