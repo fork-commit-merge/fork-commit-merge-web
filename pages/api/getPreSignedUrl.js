6f29b95e052ce1d6efeffd474647065f622d1d43
Niko Hoffrén <niko.hoffren@gmail.com>
2023-07-27 14:27:51 +0300

    Change AWS_ACCESS_KEY to AWS_ACCESS_KEY_ID for Vercel

--- pages/api/getPreSignedUrl.js
+++ pages/api/getPreSignedUrl.js
@@ -2,7 +2,7 @@
 import AWS from "aws-sdk";
 
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION,
 });
