f4f18ed65128e587af1c5e356d6e2b6e3dadfbd7
Niko Hoffrén <niko.hoffren@gmail.com>
2023-08-01 14:20:47 +0300

    Add more resource material to resources.tsx

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,18 +1,26 @@
 export default function Resources() {
     return (
         <div className="bg-slate-950 text-white py-10 px-5 flex flex-col justify-center items-center">
-            <h1 className="text-4xl font-bold mb-10">Resources</h1>
+            <h1 className="text-4xl font-bold mb-16">Resources</h1>
             <div className="flex flex-wrap justify-center">
-                <div className="w-full md:w-1/2 mb-8">
+                <div className="w-full md:w-1/2 mb-20">
                     <div className="max-w-md mx-auto">
                         <h2 className="text-2xl font-semibold mb-2">
                             Video Tutorials
                         </h2>
+                        <p className="mb-4 max-w-prose">
+                            Here you&apos;ll find a collection of video
+                            tutorials on a wide range of topics related to
+                            open-source contributions. From comprehensive guides
+                            to first-time experiences, these resources will help
+                            you understand and navigate the world of open
+                            source.
+                        </p>
                         <ul className="list-disc pl-5">
                             <li>
                                 <a
                                     href="https://www.youtube.com/watch?v=yzeVMecydCE"
-                                    className="underline text-blue-400"
+                                    className="hover:underline text-blue-400"
                                 >
                                     Complete Guide to Open Source - How to
                                     Contribute (freeCodeCamp.org)
@@ -21,7 +29,7 @@ export default function Resources() {
                             <li>
                                 <a
                                     href="https://www.youtube.com/watch?v=c6b6B9oN4Vg"
-                                    className="underline text-blue-400"
+                                    className="hover:underline text-blue-400"
                                 >
                                     Contributing to Open Source for the first
                                     time (Meta Open Source)
@@ -30,7 +38,7 @@ export default function Resources() {
                             <li>
                                 <a
                                     href="https://www.youtube.com/watch?v=2L0sxPx4C18"
-                                    className="underline text-blue-400"
+                                    className="hover:underline text-blue-400"
                                 >
                                     How to contribute to open source (strager)
                                 </a>
@@ -38,42 +46,10 @@ export default function Resources() {
                             <li>
                                 <a
                                     href="https://www.youtube.com/watch?v=LxdyX9p0wrM"
-                                    className="underline text-blue-400"
+                                    className="hover:underline text-blue-400"
                                 >
-                                    How to Contribute to Open Source Projects? (Tiff In Tech)
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="w-full md:w-1/2 mb-8">
-                    <div className="max-w-md mx-auto">
-                        <h2 className="text-2xl font-semibold mb-2">
-                            Git and GitHub Documentation
-                        </h2>
-                        <ul className="list-disc pl-5">
-                            <li>
-                                <a
-                                    href="https://git-scm.com/doc"
-                                    className="underline text-blue-400"
-                                >
-                                    Official Git Documentation
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="https://docs.github.com/en"
-                                    className="underline text-blue-400"
-                                >
-                                    Official GitHub Documentation
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="https://www.atlassian.com/git/tutorials"
-                                    className="underline text-blue-400"
-                                >
-                                    Atlassian Git Tutorial
+                                    How to Contribute to Open Source Projects?
+                                    (Tiff In Tech)
                                 </a>
                             </li>
                         </ul>
@@ -97,7 +73,7 @@ export default function Resources() {
                             <li>
                                 <a
                                     href="https://opensource.guide/"
-                                    className="underline text-blue-400"
+                                    className="hover:underline text-blue-400"
                                 >
                                     Introduction to Open Source
                                 </a>
@@ -105,7 +81,7 @@ export default function Resources() {
                             <li>
                                 <a
                                     href="https://github.com/firstcontributions/first-contributions"
-                                    className="underline text-blue-400"
+                                    className="hover:underline text-blue-400"
                                 >
                                     First Contributions on GitHub
                                 </a>
@@ -113,7 +89,7 @@ export default function Resources() {
                             <li>
                                 <a
                                     href="https://www.codecademy.com/articles/what-is-open-source"
-                                    className="underline text-blue-400"
+                                    className="hover:underline text-blue-400"
                                 >
                                     What is Open Source?
                                 </a>
@@ -121,7 +97,7 @@ export default function Resources() {
                             <li>
                                 <a
                                     href="https://opensource.org/resources"
-                                    className="underline text-blue-400"
+                                    className="hover:underline text-blue-400"
                                 >
                                     Open Source Initiative Resources
                                 </a>
@@ -132,13 +108,61 @@ export default function Resources() {
                 <div className="w-full md:w-1/2 mb-8">
                     <div className="max-w-md mx-auto">
                         <h2 className="text-2xl font-semibold mb-2">
+                            Git and GitHub Documentation
+                        </h2>
+                        <p className="mb-4 max-w-prose">
+                            Mastering Git and GitHub is fundamental for
+                            effective open-source contributions. The following
+                            documentation provides a deep dive into using Git
+                            and GitHub, including the official documentation and
+                            additional helpful tutorials.
+                        </p>
+                        <ul className="list-disc pl-5">
+                            <li>
+                                <a
+                                    href="https://git-scm.com/doc"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    Official Git Documentation
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://docs.github.com/en"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    Official GitHub Documentation
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://www.atlassian.com/git/tutorials"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    Atlassian Git Tutorial
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="w-full md:w-1/2 mb-8">
+                    <div className="max-w-md mx-auto">
+                        <h2 className="text-2xl font-semibold mb-2">
                             Interactive Learning Platforms
                         </h2>
+                        <p className="mb-4 max-w-prose">
+                            Interactive learning platforms provide a variety of
+                            courses in programming and software development.
+                            Whether you&apos;re new to coding or looking to
+                            learn a new language or skill, these resources can
+                            help you on your journey.
+                        </p>
                         <ul className="list-disc pl-5">
                             <li>
                                 <a
                                     href="https://www.codecademy.com/"
-                                    className="underline text-blue-400"
+                                    className="hover:underline text-blue-400"
                                 >
                                     Codecademy
                                 </a>
@@ -146,7 +170,7 @@ export default function Resources() {
                             <li>
                                 <a
                                     href="https://www.udemy.com/"
-                                    className="underline text-blue-400"
+                                    className="hover:underline text-blue-400"
                                 >
                                     Udemy
                                 </a>
@@ -154,9 +178,166 @@ export default function Resources() {
                             <li>
                                 <a
                                     href="https://www.coursera.org/"
-                                    className="underline text-blue-400"
+                                    className="hover:underline text-blue-400"
                                 >
                                     Coursera
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="w-full md:w-1/2 mb-8">
+                    <div className="max-w-md mx-auto">
+                        <h2 className="text-2xl font-semibold mb-2">
+                            Coding Challenges and Practice
+                        </h2>
+                        <p className="mb-4 max-w-prose">
+                            Hone your coding skills and prepare for technical
+                            interviews by solving challenges on these platforms.
+                            They offer problems of various difficulty levels in
+                            multiple programming languages.
+                        </p>
+                        <ul className="list-disc pl-5">
+                            <li>
+                                <a
+                                    href="https://www.hackerrank.com/"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    HackerRank
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://leetcode.com/"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    LeetCode
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://www.codewars.com/"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    CodeWars
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="w-full md:w-1/2 mb-8">
+                    <div className="max-w-md mx-auto">
+                        <h2 className="text-2xl font-semibold mb-2">
+                            Community and Networking
+                        </h2>
+                        <p className="mb-4 max-w-prose">
+                            Join these communities to network with other
+                            developers, ask questions, and find answers to your
+                            coding challenges. They are a great place to learn
+                            and share knowledge.
+                        </p>
+                        <ul className="list-disc pl-5">
+                            <li>
+                                <a
+                                    href="https://stackoverflow.com/"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    Stack Overflow
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://dev.to/"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    Dev.to
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://www.reddit.com/r/programming/"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    Reddit Programming
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="w-full md:w-1/2 mb-8">
+                    <div className="max-w-md mx-auto">
+                        <h2 className="text-2xl font-semibold mb-2">
+                            Job Searching Platforms
+                        </h2>
+                        <p className="mb-4 max-w-prose">
+                            Looking for your next role or just curious about
+                            what&apos;s out there? These job searching platforms
+                            are specifically tailored for tech roles and can
+                            help you find your next opportunity.
+                        </p>
+                        <ul className="list-disc pl-5">
+                            <li>
+                                <a
+                                    href="https://angel.co/jobs"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    AngelList Jobs
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://www.hired.com/"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    Hired
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://www.dice.com/"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    Dice
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="w-full md:w-1/2 mb-8">
+                    <div className="max-w-md mx-auto">
+                        <h2 className="text-2xl font-semibold mb-2">
+                            Online Coding Bootcamps
+                        </h2>
+                        <p className="mb-4 max-w-prose">
+                            Online bootcamps offer comprehensive and structured
+                            learning experiences for aspiring developers. They
+                            provide in-depth curricula and project-based
+                            learning to help you build a solid foundation in
+                            software development.
+                        </p>
+                        <ul className="list-disc pl-5">
+                            <li>
+                                <a
+                                    href="https://www.appacademy.io/"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    App Academy
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://lambdaschool.com/"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    Lambda School
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://generalassemb.ly/"
+                                    className="hover:underline text-blue-400"
+                                >
+                                    General Assembly
                                 </a>
                             </li>
                         </ul>
