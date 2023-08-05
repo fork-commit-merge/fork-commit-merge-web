fab7450405272129126fecc3bb90a537a2c7eabc
Niko Hoffrén <niko.hoffren@gmail.com>
2023-08-05 13:03:51 +0300

    Modify Header to have the home icon on the second row of the header instead of first

--- components/Header.tsx
+++ components/Header.tsx
@@ -45,18 +45,7 @@ const Header = () => {
             <nav className="bg-slate-950 text-slate-50 backdrop-blur">
                 <div className="container mx-auto px-6 py-3">
                     <div className="flex items-center justify-between">
-                        <div>
-                            <Link href="/">
-                                <div className="py-2 px-6 block text-sm cursor-pointer hover:scale-105">
-                                    <Image
-                                        src="/fork-commit-merge-logo.jpg"
-                                        alt="Fork, Commit, Merge -logo"
-                                        width={40}
-                                        height={40}
-                                    />
-                                </div>
-                            </Link>
-                        </div>
+                        <div></div>
                         <div className="md:hidden">
                             <button onClick={toggleSideNav}>
                                 {isOpen ? (
@@ -100,11 +89,17 @@ const Header = () => {
                             <Coffee />
                         </div>
                     </div>
-                    {/* <Link href="https://github.com/nikohoffren/fork-commit-merge">
-                        <div className="py-2 px-6 block text-sm cursor-pointer hover:underline hover:text-slate-100">
-                            Getting Started
-                        </div>
-                    </Link> */}
+
+                    <Link href="/">
+                                <div className="px-6 mb-2 block text-sm cursor-pointer hover:scale-105">
+                                    <Image
+                                        src="/fork-commit-merge-logo.jpg"
+                                        alt="Fork, Commit, Merge -logo"
+                                        width={40}
+                                        height={40}
+                                    />
+                                </div>
+                            </Link>
                     <Link
                         href="https://github.com/nikohoffren/fork-commit-merge"
                         target="blank"
@@ -113,6 +108,9 @@ const Header = () => {
                             Contribute in GitHub
                         </div>
                     </Link>
+
+
+
                     {/* <div className="relative" ref={node}>
                         <button
                             onClick={toggleDropdown}
@@ -162,66 +160,44 @@ const Header = () => {
             )}
             <header className="bg-slate-950 text-slate-50 sticky top-0 z-10 hidden md:block shadow-bottom pr-25">
                 <div className="mx-auto px-12 py-1 pb-6 pr-20 shadow-top">
-                    <div className="mt-3 md:flex items-center space-x-1 justify-end">
-                        {/* <div className="relative" ref={node}>
-                            <button
-                                onClick={toggleDropdown}
-                                className="py-2 px-6 block text-sm cursor-pointer hover:underline hover:text-slate-100"
-                            >
-                                Projects
-                            </button>
-                            {isDropdownOpen && (
-                                <div className="absolute left-0 w-48 py-2 mt-2 bg-slate-900 rounded-lg shadow-xl">
-                                    <Link
-                                        href="/projects/websites"
-                                        className="block px-4 py-2 text-sm cursor-pointer hover:underline hover:text-slate-100"
-                                    >
-                                        Websites
-                                    </Link>
-                                    <Link
-                                        href="/projects/mobile-apps"
-                                        className="block px-4 py-2 text-sm cursor-pointer hover:underline hover:text-slate-100"
-                                    >
-                                        Mobile Apps
-                                    </Link>
-                                    <Link
-                                        href="/projects/desktop-apps"
-                                        className="block px-4 py-2 text-sm cursor-pointer hover:underline hover:text-slate-100"
-                                    >
-                                        Desktop Apps
-                                    </Link>
+                    <div className="mt-3 md:flex items-center justify-between">
+                        <div className="flex items-center space-x-1">
+                            <Link href="/">
+                                <div className="pl-25 px-6 block text-sm cursor-pointer hover:scale-105">
+                                    <Image
+                                        src="/fork-commit-merge-logo.jpg"
+                                        alt="Fork, Commit, Merge -logo"
+                                        width={40}
+                                        height={40}
+                                    />
                                 </div>
+                            </Link>
+                        </div>
+                        <div className="flex items-center space-x-1">
+                            <Link href="/resources">
+                                <div className="py-2 px-6 block text-sm cursor-pointer hover:underline hover:text-slate-100">
+                                    Resources
+                                </div>
+                            </Link>
+                            <Link href="https://github.com/nikohoffren">
+                                <div className="py-2 pl-6 pr-12 block text-sm cursor-pointer hover:underline hover:text-slate-100">
+                                    Contact
+                                </div>
+                            </Link>
+                            {session?.user ? (
+                                <Link href="/login">
+                                    <div className="bg-slate-900 py-2 px-6 block text-sm cursor-pointer hover:bg-slate-800 hover:text-slate-100 border border-transparent font-medium rounded-md">
+                                        {session.user.name}
+                                    </div>
+                                </Link>
+                            ) : (
+                                <Link href="/login">
+                                    <div className="bg-slate-900 py-2 px-6 block text-sm cursor-pointer hover:bg-slate-800 hover:text-slate-100 border border-transparent font-medium rounded-md">
+                                        LOGIN
+                                    </div>
+                                </Link>
                             )}
-                        </div> */}
-
-                        {/* <Link href="/projects">
-                            <div className="py-2 px-6 block text-sm cursor-pointer hover:underline hover:text-slate-100">
-                                Projects
-                            </div>
-                        </Link> */}
-                        <Link href="/resources">
-                            <div className="py-2 px-6 block text-sm cursor-pointer hover:underline hover:text-slate-100">
-                                Resources
-                            </div>
-                        </Link>
-                        <Link href="https://github.com/nikohoffren">
-                            <div className="py-2 pl-6 pr-12 block text-sm cursor-pointer hover:underline hover:text-slate-100">
-                                Contact
-                            </div>
-                        </Link>
-                        {session?.user ? (
-                            <Link href="/login">
-                                <div className="bg-slate-900 py-2 px-6 block text-sm cursor-pointer hover:bg-slate-800 hover:text-slate-100 border border-transparent font-medium rounded-md">
-                                    {session.user.name}
-                                </div>
-                            </Link>
-                        ) : (
-                            <Link href="/login">
-                                <div className="bg-slate-900 py-2 px-6 block text-sm cursor-pointer hover:bg-slate-800 hover:text-slate-100 border border-transparent font-medium rounded-md">
-                                    LOGIN
-                                </div>
-                            </Link>
-                        )}
+                        </div>
                     </div>
                 </div>
             </header>
