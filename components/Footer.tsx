977eb5a74379eaecccd132cf04067b33edf07216
Niko Hoffrén <niko.hoffren@gmail.com>
2023-08-05 13:12:59 +0300

    fix: Make Footer responsive on mobile view

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 const Footer = () => {
     return (
         <>
-            <footer className="bg-slate-950 text-slate-50 pb-6 pt-10 inset-x-0 bottom-0 h-16 flex items-center justify-center">
+            <footer className="bg-slate-950 text-slate-50 pb-6 pt-10 inset-x-0 bottom-0 h-auto md:h-16 flex flex-col md:flex-row items-center justify-center">
                 <div className="container mx-auto px-6">
-                    <div className="flex justify-between items-center">
+                    <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
                         <div className="flex items-center">
                             <p className="text-sm mr-2">Deployed with</p>
                             <Link href="https://vercel.com" target="blank">
@@ -20,21 +20,19 @@ const Footer = () => {
                             </Link>
                         </div>
                         <div className="flex items-center">
-                            <p className="text-sm pr-1">
-                                Copyright © 2023</p>
-                                <Link
-                                    href="https://nikohoffren.com"
-                                    className="hover:underline text-sm text-red-100"
-                                    target="blank"
-                                >
-                                    Niko Hoffrén
-                                </Link>
-
+                            <p className="text-sm pr-1">Copyright © 2023</p>
+                            <Link
+                                href="https://nikohoffren.com"
+                                className="hover:underline text-sm text-red-100"
+                                target="blank"
+                            >
+                                Niko Hoffrén
+                            </Link>
                         </div>
-                        <div className="flex space-x-4">
+                        <div className="flex flex-col md:flex-row space-y-2 md:space-x-4">
                             <Link
                                 href="https://github.com/nikohoffren/fork-commit-merge/blob/main/CONTRIBUTING.md"
-                                className="text-sm text-slate-50 hover:underline"
+                                className="text-sm text-slate-50 hover:underline pt-2"
                                 target="blank"
                             >
                                 Contributing
