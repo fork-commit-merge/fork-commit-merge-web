10756db7c06c7f32ddfa48321e0e40bd2c739cb4
Niko Hoffrén <niko.hoffren@gmail.com>
2023-08-29 08:53:17 +0300

    Add if-statements to ignore dependabot and dependabot[bot] PR:s for not showing them in top contributors

--- pages/api/fetchTopThreeUsersByPullRequests.ts
+++ pages/api/fetchTopThreeUsersByPullRequests.ts
@@ -1,33 +1,46 @@
-import axios from 'axios';
+import axios from "axios";
 
 interface UserStat {
-  username: string;
-  prCount: number;
-  avatarUrl: string;
+    username: string;
+    prCount: number;
+    avatarUrl: string;
 }
 
-export const fetchTopThreeUsersByPullRequests = async (repo: string): Promise<UserStat[]> => {
-  const url = `https://api.github.com/repos/${repo}/pulls?state=closed&per_page=100`;
+export const fetchTopThreeUsersByPullRequests = async (
+    repo: string
+): Promise<UserStat[]> => {
+    const url = `https://api.github.com/repos/${repo}/pulls?state=closed&per_page=100`;
 
-  const response = await axios.get(url);
-  const prData = response.data;
-  const userStats: { [key: string]: { prCount: number, avatarUrl: string } } = {};
+    const response = await axios.get(url);
+    const prData = response.data;
+    const userStats: { [key: string]: { prCount: number; avatarUrl: string } } =
+        {};
 
-  prData.forEach((pr: any) => {
-    if (pr.merged_at) {
-      const username = pr.user.login;
-      const avatarUrl = pr.user.avatar_url;
-      userStats[username] = {
-        prCount: (userStats[username]?.prCount || 0) + 1,
-        avatarUrl
-      };
-    }
-  });
+    prData.forEach((pr: any) => {
+        if (pr.merged_at) {
+            const username = pr.user.login;
 
-  const sortedUsers: UserStat[] = Object.entries(userStats)
-    .sort(([, a], [, b]) => b.prCount - a.prCount)
-    .slice(0, 3)
-    .map(([username, { prCount, avatarUrl }]) => ({ username, prCount, avatarUrl }));
+            //* Ignore dependabot PRs
+            if (username === "dependabot" || username === "dependabot[bot]") {
+                return;
+            }
 
-  return sortedUsers;
+            const avatarUrl = pr.user.avatar_url;
+            userStats[username] = {
+                prCount: (userStats[username]?.prCount || 0) + 1,
+                avatarUrl,
+            };
+        }
+    });
+
+    const sortedUsers: UserStat[] = Object.entries(userStats)
+        .sort(([, a], [, b]) => b.prCount - a.prCount)
+        .slice(0, 3)
+        .map(([username, { prCount, avatarUrl }]) => ({
+            username,
+            prCount,
+            avatarUrl,
+        }));
+
+    return sortedUsers;
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -60,8 +60,9 @@ export default function Home() {
                     <div className="bg-slate-900 w-full pt-4 pb-16 text-center">
                         <div className="my-5 text-xl font-light text-gray-100 mx-auto max-w-2xl">
                             <p className="text-2xl leading-8 mt-8 mb-8">
-                                Join us with over 100 developers who have
-                                already contributed to this project!
+                                Become a part of our thriving community of over
+                                100 developers who have already contributed to
+                                this project!
                             </p>
                         </div>
                         <TopThreeContributors contributors={contributors} />
