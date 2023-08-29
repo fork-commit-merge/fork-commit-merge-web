6be921a65b52b532d30d9ef8b1447c38d970e873
Niko Hoffrén <niko.hoffren@gmail.com>
2023-08-29 15:46:06 +0300

    Add R language to badges

--- components/Badges.tsx
+++ components/Badges.tsx
@@ -203,6 +203,11 @@ const Badges: React.FC = () => {
                     className="mr-1 mb-1"
                 />
                 <img
+                    src="https://img.shields.io/badge/r-%23276DC3.svg?style=for-the-badge&logo=r&logoColor=white"
+                    alt="R"
+                    className="mr-1 mb-1"
+                />
+                <img
                     src="https://img.shields.io/badge/sql-%23FF4500.svg?style=for-the-badge&logo=sqlite&logoColor=white"
                     alt="SQL"
                     className="mr-1 mb-1"
