1f435864f80adc1f0bf9e6c13a3ef410df160801
Niko Hoffrén <niko.hoffren@gmail.com>
2023-08-31 08:47:05 +0300

    Add Google AdSense script to _document.tsx file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,38 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+} from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Ysabeau+Infant:wght@300&display=swap" rel="stylesheet" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Ysabeau+Infant:wght@300&display=swap"
+                        rel="stylesheet"
+                    />
+                    <script
+                        async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9765110233243602"
+                        crossOrigin="anonymous"
+                    ></script>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
 
-export default MyDocument
+export default MyDocument;
