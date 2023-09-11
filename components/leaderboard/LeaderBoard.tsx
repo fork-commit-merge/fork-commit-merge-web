460d26fe4bc2e761b453cab7633ec715f72819fb
Niko Hoffrén <niko.hoffren@gmail.com>
2023-09-11 10:04:33 +0300

    Refactor pages/api files

--- components/leaderboard/LeaderBoard.tsx
+++ components/leaderboard/LeaderBoard.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useState, useEffect } from "react";
 import { fetchTopUsersByPullRequests } from "../../pages/api/fetchTopUsersByPullRequests";
-import { StarFilled } from "@ant-design/icons";
 import { renderStar } from "../../utils/helpers/RenderStar";
 
 interface UserStat {
