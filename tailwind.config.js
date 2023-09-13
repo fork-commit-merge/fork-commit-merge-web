5e97164750c5091321793e779970a958fa32a36c
Niko Hoffrén <niko.hoffren@gmail.com>
2023-09-13 13:38:26 +0300

    Add wave animation to Badges.tsx and move data to components/data/badgesData.ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
     ],
     theme: {
         extend: {
+            animation: {
+                "wave-scale": "waveScale 1s infinite",
+            },
+            animationDelay: {
+                100: "100ms",
+                200: "200ms",
+                300: "300ms",
+                400: "400ms",
+                500: "500ms",
+                600: "600ms",
+                700: "700ms",
+                800: "800ms",
+                900: "900ms",
+                1000: "1000ms",
+            },
             colors: {
                 slate: {
                     920: "#191C3C",
@@ -12,7 +27,7 @@ module.exports = {
                 },
             },
             padding: {
-                '25': '7.3rem',
+                25: "7.3rem",
             },
             backdropBlur: {
                 none: "0",
@@ -23,10 +38,10 @@ module.exports = {
                 "blur-3xl": "blur(16px)",
             },
             boxShadow: {
-                bottom: "0 13px 10px -10px rgba(0.8, 0.8, 0.8, 0.8)"
+                bottom: "0 13px 10px -10px rgba(0.8, 0.8, 0.8, 0.8)",
             },
             font: {
-                primary: "Ysabeau Infant, sans-serif"
+                primary: "Ysabeau Infant, sans-serif",
             },
         },
     },
