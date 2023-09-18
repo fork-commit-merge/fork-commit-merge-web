7d5e636ac5b8d217b705d27af15a44c5e4ced8b9
Niko Hoffrén <niko.hoffren@gmail.com>
2023-09-18 16:00:14 +0300

    Add new contributors to components/data/contributorData.ts

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -1421,4 +1421,10 @@ export const contributorData = [
         "avatar": "https://avatars.githubusercontent.com/u/116346415?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
         "name": "Nasirahamad88"
     },
+    {
+        "id": "david6304",
+        "url": "https://github.com/david6304",
+        "avatar": "https://avatars.githubusercontent.com/u/73233418?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+        "name": "david6304"
+    },
 ];
