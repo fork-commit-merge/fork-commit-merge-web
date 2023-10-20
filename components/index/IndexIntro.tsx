b5a18a7ea2a92cfe4528bf132edd25ee7fca6f16
Niko Hoffrén <niko.hoffren@gmail.com>
2023-10-20 16:36:12 +0300

    Add fetchIssueCount.ts for fetching number of issues and presenting them in IndexIntro.tsx

--- components/index/IndexIntro.tsx
+++ components/index/IndexIntro.tsx
@@ -1,32 +1,39 @@
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import { fetchIssueCount } from '../../utils/fetchIssueCount'
 
 const IndexIntro: React.FC = () => {
+  const [issueCount, setIssueCount] = useState<number | null>(null)
+
+  useEffect(() => {
+    fetchIssueCount().then(count => setIssueCount(count))
+  }, [])
+
   return (
     <>
-      <div className="text-slate-50 bg-primary pb-10 w-full pt-6">
-        <div className="mx-auto max-w-2xl">
-          <h2 className="text-4xl font-bold my-4">
+      <div className='bg-primary w-full pb-10 pt-6 text-slate-50'>
+        <div className='mx-auto max-w-2xl'>
+          <h2 className='my-4 text-4xl font-bold'>
             About the Fork, Commit, Merge project
           </h2>
 
-          <p className="text-xl leading-7 pt-8">
+          <p className='pt-8 text-xl leading-7'>
             If you have already made some contributions to our Fork, Commit,
-            Merge repo, you can{" "}
-            <Link href="/login" className="underline hover:text-blue-500">
+            Merge repo, you can{' '}
+            <Link href='/login' className='underline hover:text-blue-500'>
               Login
-            </Link>{" "}
+            </Link>{' '}
             to see all of your contributions and which issues you have already
             solved. It makes it easier to keep track of your progress and see
             how you have improved your skills.
           </p>
-          <p className="text-xl leading-7 pt-8">
+          <p className='pt-8 text-xl leading-7'>
             &quot;Fork, Commit, Merge&quot; is an educational and practical
             project aimed at helping developers - both newcomers and seasoned -
             improve their skills in open-source contribution, problem-solving,
-            finding bugs, testing, and much more in over 110 individual issues
-            encompassing various programming languages, libraries and
-            frameworks.
+            finding bugs, testing, and much more in {issueCount ?? 'several'}{' '}
+            individual issues encompassing various programming languages,
+            libraries and frameworks.
             <br />
             <br />
             Here are some key aspects of the repository that make it unique and
@@ -35,7 +42,7 @@ const IndexIntro: React.FC = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default IndexIntro;
+export default IndexIntro
