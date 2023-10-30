128a9cb7b01aacd4f7c12f6cd1c3fec559a528eb
Niko Hoffrén <niko.hoffren@gmail.com>
2023-10-30 21:32:51 +0200

    Add AlexHoop140 to components/data/contributorData.ts

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -3202,4 +3202,10 @@ export const contributorData = [
     "avatar": "https://avatars.githubusercontent.com/u/108383726?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     "name": "locdeptraihehe"
   },
+  {
+    "id": "AlexHoop140",
+    "url": "https://github.com/AlexHoop140",
+    "avatar": "https://avatars.githubusercontent.com/u/30748832?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    "name": "AlexHoop140"
+  },
 ];
