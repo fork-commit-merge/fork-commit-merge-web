a826dd80f4c173af17448d730ef7725e925c73c1
Niko Hoffrén <niko.hoffren@gmail.com>
2023-11-02 08:27:39 +0200

    Remove unnecessary React fragment in IndexIntro.tsx

--- components/index/IndexIntro.tsx
+++ components/index/IndexIntro.tsx
@@ -17,38 +17,36 @@ const IndexIntro: React.FC = () => {
   }
 
   return (
-    <>
-      <div className='bg-primary w-full pb-10 pt-6 '>
-        <div className='mx-auto max-w-2xl'>
-          <h2 className='my-4 text-4xl font-bold'>
-            About the Fork, Commit, Merge project
-          </h2>
+    <div className='bg-primary w-full pb-10 pt-6 '>
+      <div className='mx-auto max-w-2xl'>
+        <h2 className='my-4 text-4xl font-bold'>
+          About the Fork, Commit, Merge project
+        </h2>
 
-          <p className='pt-8 text-xl leading-7'>
-            If you have already made some contributions to our Fork, Commit,
-            Merge repo, you can{' '}
-            <Link href='/login' className='underline hover:text-blue-500'>
-              Login
-            </Link>{' '}
-            to see all of your contributions and which issues you have already
-            solved. It makes it easier to keep track of your progress and see
-            how you have improved your skills.
-          </p>
-          <p className='pt-8 text-xl leading-7'>
-            &quot;Fork, Commit, Merge&quot; is an educational and practical
-            project aimed at helping developers - both newcomers and seasoned -
-            improve their skills in open-source contribution, problem-solving,
-            finding bugs, testing, and much more in {displayIssueCount()}{' '}
-            individual issues encompassing various programming languages,
-            libraries and frameworks.
-            <br />
-            <br />
-            Here are some key aspects of the repository that make it unique and
-            enriching for every developer:
-          </p>
-        </div>
+        <p className='pt-8 text-xl leading-7'>
+          If you have already made some contributions to our Fork, Commit, Merge
+          repo, you can{' '}
+          <Link href='/login' className='underline hover:text-blue-500'>
+            Login
+          </Link>{' '}
+          to see all of your contributions and which issues you have already
+          solved. It makes it easier to keep track of your progress and see how
+          you have improved your skills.
+        </p>
+        <p className='pt-8 text-xl leading-7'>
+          &quot;Fork, Commit, Merge&quot; is an educational and practical
+          project aimed at helping developers - both newcomers and seasoned -
+          improve their skills in open-source contribution, problem-solving,
+          finding bugs, testing, and much more in {displayIssueCount()}{' '}
+          individual issues encompassing various programming languages,
+          libraries and frameworks.
+          <br />
+          <br />
+          Here are some key aspects of the repository that make it unique and
+          enriching for every developer:
+        </p>
       </div>
-    </>
+    </div>
   )
 }
 
