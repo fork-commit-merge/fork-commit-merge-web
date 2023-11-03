ac4bffe676cb7e4a7c447ad987e03a8bdc5c1ac4
Niko Hoffrén <niko.hoffren@gmail.com>
2023-11-03 15:01:55 +0200

    Add Mojo and Kubernetes badges to components/data/badgesData.ts and README.md

--- components/data/badgesData.ts
+++ components/data/badgesData.ts
@@ -316,6 +316,10 @@ export const badgesData = [
     alt: "Perl",
   },
   {
+    src: "https://img.shields.io/badge/Mojo-FF7139?style=for-the-badge&logo=Mojo&logoColor=white",
+    alt: "Mojo",
+  },
+  {
     src: "https://img.shields.io/badge/asm-%23121011.svg?style=for-the-badge&logo=asm&logoColor=white",
     alt: "ASM",
   },
@@ -350,6 +354,10 @@ export const badgesData = [
   {
     src: "https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white",
     alt: "Docker",
+  },
+  {
+    src: "https://img.shields.io/badge/kubernetes-%23326ce5.svg?style=for-the-badge&logo=kubernetes&logoColor=white",
+    alt: "Kubernetes",
   },
   {
     src: "https://img.shields.io/badge/git-%23000000.svg?style=for-the-badge&logo=git&logoColor=white",
