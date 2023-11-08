98f50a2a7af2bfd12be104d75fbb053ca9d2ca4b
Niko Hoffrén <niko.hoffren@gmail.com>
2023-11-08 13:36:45 +0200

    Remove Adventures section from Community

--- components/data/communityList.ts
+++ components/data/communityList.ts
@@ -1,5 +1,4 @@
 export const communityList = [
   { name: "Discussions", link: "/community/discussions" },
   { name: "Events", link: "/community/events" },
-  { name: "Adventures", link: "/community/adventures" },
 ];
--- pages/community/[community].tsx
+++ pages/community/[community].tsx
@@ -1,32 +1,26 @@
-import { useRouter } from "next/router";
-import React from "react";
-import Discussions from "../../components/communityComponents/Discussions";
-import Events from "../../components/communityComponents/Events";
-import Adventures from "../../components/communityComponents/Adventures";
-import CommunityNotSupported from "../../components/communityComponents/CommunityNotSupported";
+import { useRouter } from 'next/router'
+import React from 'react'
+import Discussions from '../../components/communityComponents/Discussions'
+import Events from '../../components/communityComponents/Events'
+import CommunityNotSupported from '../../components/communityComponents/CommunityNotSupported'
 
 const CommunityPage = () => {
-  const router = useRouter();
-  const { community } = router.query;
+  const router = useRouter()
+  const { community } = router.query
 
   const renderContent = () => {
     switch (community) {
-      case "discussions":
-        return <Discussions />;
-      case "events":
-        return <Events />;
-      case "adventures":
-        return <Adventures />;
+      case 'discussions':
+        return <Discussions />
+      case 'events':
+        return <Events />
+
       default:
-        return <CommunityNotSupported />;
+        return <CommunityNotSupported />
     }
-  };
+  }
 
-  return (
-    <>
-      <div className="bg-primary py-10 px-6">{renderContent()}</div>
-    </>
-  );
-};
+  return <div className='bg-primary px-6 py-10'>{renderContent()}</div>
+}
 
-export default CommunityPage;
+export default CommunityPage
