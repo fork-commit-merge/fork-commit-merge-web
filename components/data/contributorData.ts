9511dfe89b6964d443599945ac4292e5a125c140
Niko Hoffrén <niko.hoffren@gmail.com>
2025-03-11 09:19:03 +0200

    Add new contributors to contributorData

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -5375,4 +5375,18 @@ export const contributorData = [
       'https://avatars.githubusercontent.com/u/151425409?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d',
     name: 'risu043'
   },
+  {
+    id: 'zshaian',
+    url: 'https://github.com/zshaian',
+    avatar:
+      'https://avatars.githubusercontent.com/u/81682916?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d',
+    name: 'zshaian'
+  },
+  {
+    id: 'amanahsan01',
+    url: 'https://github.com/amanahsan01',
+    avatar:
+      'https://avatars.githubusercontent.com/u/96137455?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d',
+    name: 'amanahsan01'
+  },
 ]
