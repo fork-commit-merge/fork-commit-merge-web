80876ba37d962d41ed9bc81882124720f14e0b7b
Niko Hoffrén <niko.hoffren@gmail.com>
2025-03-11 09:15:14 +0200

    Add amanahsan01 to testimonials

--- components/data/testimonials.ts
+++ components/data/testimonials.ts
@@ -1,83 +1,89 @@
-import { Testimonial as TestimonialType } from "../../types";
+import { Testimonial as TestimonialType } from '../../types'
 
 export const testimonials: TestimonialType[] = [
   {
     text: 'Love these "real world" scenario tasks to help new developers. Kudos on this project!!',
-    name: "jaas666",
-    imageUrl: "https://avatars.githubusercontent.com/u/30204147?v=4",
-    url: "https://github.com/jaas666",
+    name: 'jaas666',
+    imageUrl: 'https://avatars.githubusercontent.com/u/30204147?v=4',
+    url: 'https://github.com/jaas666'
   },
   {
     text: "I've tried contributing to open source since a year but could never successfully change the code and get my pull request merged in other projects. But this project actually made me understand how to contribute. I would suggest this to all my friends and juniors in the future.",
-    name: "Layak-Ali",
-    imageUrl: "https://avatars.githubusercontent.com/u/93964740?v=4",
-    url: "https://github.com/Layak-Ali",
+    name: 'Layak-Ali',
+    imageUrl: 'https://avatars.githubusercontent.com/u/93964740?v=4',
+    url: 'https://github.com/Layak-Ali'
   },
   {
-    text: "Loved your repo. I m not a developer but started using GitHub to learn what this amazing website is and how can I be part of software community with over 100 million developers on it. I was quite taken aback when I discovered this.",
-    name: "usmangdeveloper",
-    imageUrl: "https://avatars.githubusercontent.com/u/138635923?v=4",
-    url: "https://github.com/usmangdeveloper",
+    text: 'Loved your repo. I m not a developer but started using GitHub to learn what this amazing website is and how can I be part of software community with over 100 million developers on it. I was quite taken aback when I discovered this.',
+    name: 'usmangdeveloper',
+    imageUrl: 'https://avatars.githubusercontent.com/u/138635923?v=4',
+    url: 'https://github.com/usmangdeveloper'
   },
   {
-    text: "Thank you for giving us this opportunity where beginners can start to contribute to open source.",
-    name: "Saloni581",
-    imageUrl: "https://avatars.githubusercontent.com/u/125601277?v=4",
-    url: "https://github.com/Saloni581",
+    text: 'Thank you for giving us this opportunity where beginners can start to contribute to open source.',
+    name: 'Saloni581',
+    imageUrl: 'https://avatars.githubusercontent.com/u/125601277?v=4',
+    url: 'https://github.com/Saloni581'
   },
   {
     text: "You're doing a great work for beginners like me. I'll be spending some more time with your repo to get familiar with open source contributions.",
-    name: "nshnt777",
-    imageUrl: "https://avatars.githubusercontent.com/u/96719523?v=4",
-    url: "https://github.com/nshnt777",
+    name: 'nshnt777',
+    imageUrl: 'https://avatars.githubusercontent.com/u/96719523?v=4',
+    url: 'https://github.com/nshnt777'
   },
   {
-    text: "Thank you, you are doing a great job. :-)",
-    name: "kajalverma0101",
-    imageUrl: "https://avatars.githubusercontent.com/u/88632476?v=4",
-    url: "https://github.com/kajalverma0101",
+    text: 'Thank you, you are doing a great job. :-)',
+    name: 'kajalverma0101',
+    imageUrl: 'https://avatars.githubusercontent.com/u/88632476?v=4',
+    url: 'https://github.com/kajalverma0101'
   },
   {
-    text: "Great experiences.",
-    name: "Nasirahamad88",
+    text: 'Great experiences.',
+    name: 'Nasirahamad88',
     imageUrl:
-      "https://avatars.githubusercontent.com/u/116346415?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
-    url: "https://github.com/Nasirahamad88",
+      'https://avatars.githubusercontent.com/u/116346415?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d',
+    url: 'https://github.com/Nasirahamad88'
   },
   {
-    text: "Thanks for making this, such a lovely 1st time experience contributing.",
-    name: "Gourav2609",
-    imageUrl: "https://avatars.githubusercontent.com/u/115522947?v=4",
-    url: "https://github.com/Gourav2609",
+    text: 'Thanks for making this, such a lovely 1st time experience contributing.',
+    name: 'Gourav2609',
+    imageUrl: 'https://avatars.githubusercontent.com/u/115522947?v=4',
+    url: 'https://github.com/Gourav2609'
   },
   {
-    text: "Thank you for creating such a great way for new contributors to get used to making open source commits. I am a TS/React developer trying to relearn the Python I learned in college a while ago and this is such an easy and fun way to practice that! Kudos to you and everyone else involved in making this repo!",
-    name: "DustyDogCodex",
-    imageUrl: "https://avatars.githubusercontent.com/u/72352123?v=4",
-    url: "https://github.com/DustyDogCodex",
+    text: 'Thank you for creating such a great way for new contributors to get used to making open source commits. I am a TS/React developer trying to relearn the Python I learned in college a while ago and this is such an easy and fun way to practice that! Kudos to you and everyone else involved in making this repo!',
+    name: 'DustyDogCodex',
+    imageUrl: 'https://avatars.githubusercontent.com/u/72352123?v=4',
+    url: 'https://github.com/DustyDogCodex'
   },
   {
     text: "I'm getting interested into contributing to open source and this was a nice way of learning how to do so. I will keep trying to a few ones before getting into other open-source projects to make sure I do it well!",
-    name: "ClearlyHere",
-    imageUrl: "https://avatars.githubusercontent.com/u/133259204?v=4",
-    url: "https://github.com/ClearlyHere",
+    name: 'ClearlyHere',
+    imageUrl: 'https://avatars.githubusercontent.com/u/133259204?v=4',
+    url: 'https://github.com/ClearlyHere'
   },
   {
-    text: "Starting my journey with Node.js and open source - gaining confidence, one contribution at a time. Thanks to this awesome community!",
-    name: "Dhanesh539",
-    imageUrl: "https://avatars.githubusercontent.com/u/100354896?v=4",
-    url: "https://github.com/Dhanesh539",
+    text: 'Starting my journey with Node.js and open source - gaining confidence, one contribution at a time. Thanks to this awesome community!',
+    name: 'Dhanesh539',
+    imageUrl: 'https://avatars.githubusercontent.com/u/100354896?v=4',
+    url: 'https://github.com/Dhanesh539'
   },
   {
-    text: "Thank you so much for such an accessible and easy way of getting into open source!",
-    name: "TheHolyRoller",
-    imageUrl: "https://avatars.githubusercontent.com/u/79761726?v=4",
-    url: "https://github.com/TheHolyRoller",
+    text: 'Thank you so much for such an accessible and easy way of getting into open source!',
+    name: 'TheHolyRoller',
+    imageUrl: 'https://avatars.githubusercontent.com/u/79761726?v=4',
+    url: 'https://github.com/TheHolyRoller'
   },
   {
-    text: "Thank you for the support! This is a great community.",
-    name: "harishankarr7",
-    imageUrl: "https://avatars.githubusercontent.com/u/31157448?v=4",
-    url: "https://github.com/harishankarr7",
+    text: 'Thank you for the support! This is a great community.',
+    name: 'harishankarr7',
+    imageUrl: 'https://avatars.githubusercontent.com/u/31157448?v=4',
+    url: 'https://github.com/harishankarr7'
   },
-];
+  {
+    text: `Just starting out on my open source journey :) Thank you to everyone who worked on this project.`,
+    name: 'amanahsan01',
+    imageUrl: 'https://avatars.githubusercontent.com/u/96137455?v=4',
+    url: 'https://github.com/amanahsan01'
+  },
+]
