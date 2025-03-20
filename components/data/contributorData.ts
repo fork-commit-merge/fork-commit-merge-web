a29ef4761e9e621541e8548c85fc5216a95d1cc3
Niko Hoffrén <niko.hoffren@gmail.com>
2025-03-20 20:14:35 +0200

    Update contributorData.ts

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -5418,18 +5418,6 @@ export const contributorData = [
     name: 'pkj-web'
   },
   {
-    id: "initialstate86",
-    url: "https://github.com/initialstate86",
-    avatar: "https://avatars.githubusercontent.com/u/204231046?v=4  {
-    id: "mradul-001",
-    url: "https://github.com/mradul-001",
-    avatar: "https://avatars.githubusercontent.com/u/140724798?v=4]h=300]w=300]fit=cover]mask=circle]maxage=7d",
-    name: "mradul-001"
-  },
-]h=300]w=300]fit=cover]mask=circle]maxage=7d",
-    name: "initialstate86"
-  },
-  {
     id: "mradul-001",
     url: "https://github.com/mradul-001",
     avatar: "https://avatars.githubusercontent.com/u/140724798?v=4]h=300]w=300]fit=cover]mask=circle]maxage=7d",
