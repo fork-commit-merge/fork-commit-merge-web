a7b68f19c4489fdeec8a2e5003a5168d25a237ee
GitHub Action <action@github.com>
2025-03-20 17:49:20 +0000

    Add initialstate86 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -56,11 +56,29 @@ export const contributorData = [
     name: 'Rajdeep1311'
   },
   {
-    id: 'dependabot[bot]',
-    url: 'https://github.com/dependabot[bot]',
+    id: 'dependabot[bot  {
+    id: "initialstate86",
+    url: "https://github.com/initialstate86",
+    avatar: "https://avatars.githubusercontent.com/u/204231046?v=4]h=300]w=300]fit=cover]mask=circle]maxage=7d",
+    name: "initialstate86"
+  },
+]',
+    url: 'https://github.com/dependabot[bot  {
+    id: "initialstate86",
+    url: "https://github.com/initialstate86",
+    avatar: "https://avatars.githubusercontent.com/u/204231046?v=4]h=300]w=300]fit=cover]mask=circle]maxage=7d",
+    name: "initialstate86"
+  },
+]',
     avatar:
       'https://avatars.githubusercontent.com/u/49699333?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d',
-    name: 'dependabot[bot]'
+    name: 'dependabot[bot  {
+    id: "initialstate86",
+    url: "https://github.com/initialstate86",
+    avatar: "https://avatars.githubusercontent.com/u/204231046?v=4]h=300]w=300]fit=cover]mask=circle]maxage=7d",
+    name: "initialstate86"
+  },
+]'
   },
   {
     id: 'DarshanDixit05',
@@ -5423,5 +5441,11 @@ export const contributorData = [
     avatar:
       'https://avatars.githubusercontent.com/u/130211933?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d',
     name: 'pkj-web'
+  },
+  {
+    id: "initialstate86",
+    url: "https://github.com/initialstate86",
+    avatar: "https://avatars.githubusercontent.com/u/204231046?v=4]h=300]w=300]fit=cover]mask=circle]maxage=7d",
+    name: "initialstate86"
   },
 ]
