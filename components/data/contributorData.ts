d955cebce5789b102bce23bd1cafcd80163d075c
GitHub Action <action@github.com>
2025-03-21 05:41:48 +0000

    Add initialstate86 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -5435,4 +5435,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/204231046?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "initialstate86"
   },
+  {
+    id: "initialstate86",
+    url: "https://github.com/initialstate86",
+    avatar: "https://avatars.githubusercontent.com/u/204231046?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "initialstate86"
+  },
 ]
