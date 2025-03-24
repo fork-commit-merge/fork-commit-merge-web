3cdfe79804fe1060636ae63f9d2f500bff812917
GitHub Action <action@github.com>
2025-03-24 05:40:17 +0000

    Add BotirBakhtiyarov to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -5459,4 +5459,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/26349388?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "kbrooks81"
   },
+  {
+    id: "BotirBakhtiyarov",
+    url: "https://github.com/BotirBakhtiyarov",
+    avatar: "https://avatars.githubusercontent.com/u/68393895?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "BotirBakhtiyarov"
+  },
 ]
