bd140f2ba7b58af3779deccbfb509b7aaaaf9ad4
Niko Hoffrén <niko.hoffren@gmail.com>
2025-03-25 12:44:14 +0200

    feat: add Leaderboard functionality

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -36,7 +36,7 @@ const LeaderBoardPage: React.FC = () => {
               Top 20 Contributors by Solved Issues
             </h2>
             <p className='pb-10 pt-2 italic text-gray-400'>
-              Bot will update this list once a day.
+              Bot will update this list once a week.
             </p>
             <LeaderBoard />
             <Stars />
