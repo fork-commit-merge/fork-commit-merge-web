5f48ef3c26fd1e9105b730c32dbdf8e3e88c6d73
GitHub Action <action@github.com>
2025-04-02 18:36:07 +0000

    Add SurajSadhula to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -5501,4 +5501,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/128107374?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "netscrawler"
   },
+  {
+    id: "SurajSadhula",
+    url: "https://github.com/SurajSadhula",
+    avatar: "https://avatars.githubusercontent.com/u/181082867?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "SurajSadhula"
+  },
 ]
