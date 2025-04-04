14e31079e65cdf6d4c312250f7e3feb314ea1905
Niko Hoffrén <niko.hoffren@gmail.com>
2025-04-04 14:43:05 +0300

    remove unnecessary comments from the loading page

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -7,18 +7,15 @@ export default function DashboardPage() {
   const { isLoaded, isSignedIn } = useUser()
   const router = useRouter()
 
-  // If not loaded yet, show loading page
   if (!isLoaded) {
     return <LoadingPage />
   }
 
-  // If user is not signed in, redirect to sign-in
   if (!isSignedIn) {
     router.push('/sign-in')
     return null
   }
 
-  // If user is signed in, show loading page (which will redirect to home)
   return <LoadingPage />
 }
 
