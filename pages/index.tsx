34a1b7c3e812303432bb161e23bbe7ab869d74ee
Niko Hoffrén <niko.hoffren@gmail.com>
2025-04-23 14:39:32 +0300

    update issue count

--- pages/index.tsx
+++ pages/index.tsx
@@ -97,7 +97,7 @@ export default function Home() {
                   aimed at helping developers - both newcomers and seasoned -
                   improve their skills in open-source contribution,
                   problem-solving, finding bugs, testing, and much more in{' '}
-                  <span className='text-green-400'>135</span> individual issues
+                  <span className='text-green-400'>134</span> individual issues
                   encompassing various programming languages, libraries and
                   frameworks.
                 </p>
