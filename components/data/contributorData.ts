b7b60e5405169cc58067bfb478c81382ec390908
GitHub Action <action@github.com>
2025-05-01 10:32:59 +0000

    Add TalhaJubaerPrantor to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -5651,4 +5651,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/169197115?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "ctorres4564"
   },
+  {
+    id: "TalhaJubaerPrantor",
+    url: "https://github.com/TalhaJubaerPrantor",
+    avatar: "https://avatars.githubusercontent.com/u/171762716?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "TalhaJubaerPrantor"
+  },
 ]
