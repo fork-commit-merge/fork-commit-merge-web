8e079363bc68fadbb9fd80cd6834c6241311431c
GitHub Action <action@github.com>
2025-05-05 06:38:10 +0000

    Add UmarNasib to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -5705,4 +5705,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/11198838?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "dbto"
   },
+  {
+    id: "UmarNasib",
+    url: "https://github.com/UmarNasib",
+    avatar: "https://avatars.githubusercontent.com/u/16023298?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "UmarNasib"
+  },
 ]
