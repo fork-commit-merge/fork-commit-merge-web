8dea311a27caecccd64a22d36c22f60eb28c3195
GitHub Action <action@github.com>
2025-05-13 07:30:52 +0000

    Add moteeb-asad to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -5759,4 +5759,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/52282002?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "AppiumMal"
   },
+  {
+    id: "moteeb-asad",
+    url: "https://github.com/moteeb-asad",
+    avatar: "https://avatars.githubusercontent.com/u/112121704?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "moteeb-asad"
+  },
 ]
