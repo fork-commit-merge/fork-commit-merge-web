b04bdce384b638a6fc64be748a9417f6ac61c398
GitHub Action <action@github.com>
2025-06-01 15:20:14 +0000

    Add abdelrahim3aa to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -5904,4 +5904,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/188657775?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "kroej"
   },
+  {
+    id: "abdelrahim3aa",
+    url: "https://github.com/abdelrahim3aa",
+    avatar: "https://avatars.githubusercontent.com/u/138967079?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "abdelrahim3aa"
+  },
 ]
