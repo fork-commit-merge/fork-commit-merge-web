4a17e78ae95d31a0dd7986677de9254676012522
GitHub Action <action@github.com>
2025-06-09 03:32:53 +0000

    Add Bakari-K to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -5958,4 +5958,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/146610552?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "xankush"
   },
+  {
+    id: "Bakari-K",
+    url: "https://github.com/Bakari-K",
+    avatar: "https://avatars.githubusercontent.com/u/200834907?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "Bakari-K"
+  },
 ]
