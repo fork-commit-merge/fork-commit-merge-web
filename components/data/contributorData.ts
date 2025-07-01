49878a0aba6d05149721b6b955f26fd3c7b1cf8d
GitHub Action <action@github.com>
2025-07-01 08:59:15 +0000

    Add Devansh501 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -6066,4 +6066,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/162747234?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "hurtki"
   },
+  {
+    id: "Devansh501",
+    url: "https://github.com/Devansh501",
+    avatar: "https://avatars.githubusercontent.com/u/81950589?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "Devansh501"
+  },
 ]
