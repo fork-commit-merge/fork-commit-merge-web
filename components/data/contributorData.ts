a8801b08ee08af37648f11f5ebc11b476aca3455
GitHub Action <action@github.com>
2025-07-27 12:54:53 +0000

    Add ammysharma11 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -6264,4 +6264,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/91038290?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "RonydaEssam"
   },
+  {
+    id: "ammysharma11",
+    url: "https://github.com/ammysharma11",
+    avatar: "https://avatars.githubusercontent.com/u/108935281?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "ammysharma11"
+  },
 ]
