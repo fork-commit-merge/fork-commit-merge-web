aafcc1790377efabd8d5a2ece07e403e54aa1c42
GitHub Action <action@github.com>
2025-08-24 08:49:25 +0000

    Add sbera13 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -6420,4 +6420,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/188760963?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Roma3214185"
   },
+  {
+    id: "sbera13",
+    url: "https://github.com/sbera13",
+    avatar: "https://avatars.githubusercontent.com/u/52022700?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "sbera13"
+  },
 ]
