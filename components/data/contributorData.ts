547218d019075cb5a928a33e0cdcea3f1e57a868
GitHub Action <action@github.com>
2025-09-01 08:29:07 +0000

    Add mariyam-hanna to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -6438,4 +6438,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/99832312?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "p-alvarenga"
   },
+  {
+    id: "mariyam-hanna",
+    url: "https://github.com/mariyam-hanna",
+    avatar: "https://avatars.githubusercontent.com/u/132428985?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "mariyam-hanna"
+  },
 ]
