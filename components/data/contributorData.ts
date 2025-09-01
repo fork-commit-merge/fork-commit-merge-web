aca784de2451e0ad726af2e93a6f7c8205b92867
GitHub Action <action@github.com>
2025-09-01 12:37:02 +0000

    Add Prabhu-1409 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -6450,4 +6450,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/103478170?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Valcyle"
   },
+  {
+    id: "Prabhu-1409",
+    url: "https://github.com/Prabhu-1409",
+    avatar: "https://avatars.githubusercontent.com/u/106946918?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "Prabhu-1409"
+  },
 ]
