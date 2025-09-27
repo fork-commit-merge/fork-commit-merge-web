0e355c2f38854b5b6b5ea794a9d30a24e7f501b0
GitHub Action <action@github.com>
2025-09-27 05:25:05 +0000

    Add Mohamedan-Farag to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -6618,4 +6618,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/113913573?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "patraarka"
   },
+  {
+    id: "Mohamedan-Farag",
+    url: "https://github.com/Mohamedan-Farag",
+    avatar: "https://avatars.githubusercontent.com/u/120517880?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "Mohamedan-Farag"
+  },
 ]
