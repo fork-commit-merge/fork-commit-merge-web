45f34241b897047a0c3b2b6fd6e47d6ceb9f5be7
GitHub Action <action@github.com>
2025-11-03 15:40:46 +0000

    Add Aravind30648 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -6786,4 +6786,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/157124771?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "MaelBallereau"
   },
+  {
+    id: "Aravind30648",
+    url: "https://github.com/Aravind30648",
+    avatar: "https://avatars.githubusercontent.com/u/221258979?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "Aravind30648"
+  },
 ]
