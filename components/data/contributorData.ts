5c3b7feb1a94e666cfd9b0261270d1b9294fcb4d
GitHub Action <action@github.com>
2025-11-21 06:07:03 +0000

    Add Hamidou3110 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -6918,4 +6918,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/100580313?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Ghaith-Saidani"
   },
+  {
+    id: "Hamidou3110",
+    url: "https://github.com/Hamidou3110",
+    avatar: "https://avatars.githubusercontent.com/u/206198208?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "Hamidou3110"
+  },
 ]
