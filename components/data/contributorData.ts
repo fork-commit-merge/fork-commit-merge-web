c9192031393d94f975e76d15a1462d710b456322
GitHub Action <action@github.com>
2025-11-29 09:18:50 +0000

    Add Manthancode28 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7038,4 +7038,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/168280442?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Ronak501"
   },
+  {
+    id: "Manthancode28",
+    url: "https://github.com/Manthancode28",
+    avatar: "https://avatars.githubusercontent.com/u/152332786?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "Manthancode28"
+  },
 ]
