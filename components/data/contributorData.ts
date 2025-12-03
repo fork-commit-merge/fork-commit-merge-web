80ce2c1feea261a1ebe32f8256706092b19d74ef
GitHub Action <action@github.com>
2025-12-03 07:46:29 +0000

    Add Illiadeveloper to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7086,4 +7086,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/24905481?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "znecesito"
   },
+  {
+    id: "Illiadeveloper",
+    url: "https://github.com/Illiadeveloper",
+    avatar: "https://avatars.githubusercontent.com/u/157731687?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "Illiadeveloper"
+  },
 ]
