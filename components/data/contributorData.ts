d0ba45fd0f0df9c8cf74d1648e6baaab41a3233a
GitHub Action <action@github.com>
2025-12-15 18:28:29 +0000

    Add adityav27 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7128,4 +7128,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/92822833?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Manoo089"
   },
+  {
+    id: "adityav27",
+    url: "https://github.com/adityav27",
+    avatar: "https://avatars.githubusercontent.com/u/194900185?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "adityav27"
+  },
 ]
