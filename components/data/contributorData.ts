28c7df639b1eb3cd17153b0684a4eb035bb6535a
GitHub Action <action@github.com>
2025-12-22 10:49:03 +0000

    Add hyunjunko01 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7200,4 +7200,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/161838111?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "jayalloyd"
   },
+  {
+    id: "hyunjunko01",
+    url: "https://github.com/hyunjunko01",
+    avatar: "https://avatars.githubusercontent.com/u/61777115?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "hyunjunko01"
+  },
 ]
