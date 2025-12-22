c4301227392286202c9aff0f17ac5a4be4f97bb2
GitHub Action <action@github.com>
2025-12-22 05:33:51 +0000

    Add jayalloyd to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7194,4 +7194,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/95329958?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Ashpawarash1"
   },
+  {
+    id: "jayalloyd",
+    url: "https://github.com/jayalloyd",
+    avatar: "https://avatars.githubusercontent.com/u/161838111?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "jayalloyd"
+  },
 ]
