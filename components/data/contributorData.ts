5c99a5c3b7b1ff521cbfc014915fd83e113b2fc3
GitHub Action <action@github.com>
2026-01-03 11:35:18 +0000

    Add fadigatardzenyuy to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7290,4 +7290,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/229804345?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "akshay-it23"
   },
+  {
+    id: "fadigatardzenyuy",
+    url: "https://github.com/fadigatardzenyuy",
+    avatar: "https://avatars.githubusercontent.com/u/164689751?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "fadigatardzenyuy"
+  },
 ]
