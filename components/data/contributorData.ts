e666efba715d4e9e6e75ae233be10f337b4b49de
GitHub Action <action@github.com>
2026-01-07 15:44:41 +0000

    Add ynamkit13 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7350,4 +7350,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/196148526?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Hetg411"
   },
+  {
+    id: "ynamkit13",
+    url: "https://github.com/ynamkit13",
+    avatar: "https://avatars.githubusercontent.com/u/214979409?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "ynamkit13"
+  },
 ]
