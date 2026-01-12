3b8f9c7aa563a8fb16d29379379da47224bf33a6
GitHub Action <action@github.com>
2026-01-12 06:06:31 +0000

    Add Vlakiz to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7386,4 +7386,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/36754286?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "gmnabeel"
   },
+  {
+    id: "Vlakiz",
+    url: "https://github.com/Vlakiz",
+    avatar: "https://avatars.githubusercontent.com/u/46784289?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "Vlakiz"
+  },
 ]
