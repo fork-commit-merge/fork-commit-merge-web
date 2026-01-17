5cd5ce3699462776e57a43fd33aa7eae6784eaab
GitHub Action <action@github.com>
2026-01-17 06:39:24 +0000

    Add Alauddinbukhari to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7392,4 +7392,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/46784289?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Vlakiz"
   },
+  {
+    id: "Alauddinbukhari",
+    url: "https://github.com/Alauddinbukhari",
+    avatar: "https://avatars.githubusercontent.com/u/56410694?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "Alauddinbukhari"
+  },
 ]
