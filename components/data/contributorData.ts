8811037da1825c40ae7ee4b2b1c811e1475f2e7c
GitHub Action <action@github.com>
2026-01-20 06:46:41 +0000

    Add muhammadhassanmahar to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7404,4 +7404,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/108528691?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "YKorniienkova"
   },
+  {
+    id: "muhammadhassanmahar",
+    url: "https://github.com/muhammadhassanmahar",
+    avatar: "https://avatars.githubusercontent.com/u/212078171?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "muhammadhassanmahar"
+  },
 ]
