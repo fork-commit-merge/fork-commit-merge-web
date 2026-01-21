edbf2261064f90d001c2604792c3d5a6ef0776d1
GitHub Action <action@github.com>
2026-01-21 22:11:45 +0000

    Add gialampou to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7410,4 +7410,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/212078171?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "muhammadhassanmahar"
   },
+  {
+    id: "gialampou",
+    url: "https://github.com/gialampou",
+    avatar: "https://avatars.githubusercontent.com/u/147716645?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "gialampou"
+  },
 ]
