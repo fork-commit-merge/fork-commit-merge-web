89e1be5a5e54e4a6a0b0c501dd3fe955e9795475
GitHub Action <action@github.com>
2026-01-29 07:07:56 +0000

    Add abdussamad567 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7422,4 +7422,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/147716753?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "dagioups"
   },
+  {
+    id: "abdussamad567",
+    url: "https://github.com/abdussamad567",
+    avatar: "https://avatars.githubusercontent.com/u/219780238?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "abdussamad567"
+  },
 ]
