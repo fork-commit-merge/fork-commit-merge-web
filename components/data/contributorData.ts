e5d39cf1492c984a37cea9e503ee36249785a5bc
GitHub Action <action@github.com>
2026-02-05 15:12:31 +0000

    Add lavanitha to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7470,4 +7470,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/79399513?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "AbhiSahane0"
   },
+  {
+    id: "lavanitha",
+    url: "https://github.com/lavanitha",
+    avatar: "https://avatars.githubusercontent.com/u/202476260?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "lavanitha"
+  },
 ]
