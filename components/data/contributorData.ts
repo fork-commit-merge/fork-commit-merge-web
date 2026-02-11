47ba734876bae0d6283fbdb3cec17f4196556718
GitHub Action <action@github.com>
2026-02-11 18:55:22 +0000

    Add ritikaa1802 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7512,4 +7512,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/93976924?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "GRICHChaimae"
   },
+  {
+    id: "ritikaa1802",
+    url: "https://github.com/ritikaa1802",
+    avatar: "https://avatars.githubusercontent.com/u/214799084?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "ritikaa1802"
+  },
 ]
