d0f465bdce37efbfdda7382b73fa0ab47427ff0d
GitHub Action <action@github.com>
2026-03-10 13:13:11 +0000

    Add Dhanjith01 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7608,4 +7608,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/104318417?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Lichu23"
   },
+  {
+    id: "Dhanjith01",
+    url: "https://github.com/Dhanjith01",
+    avatar: "https://avatars.githubusercontent.com/u/172830598?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "Dhanjith01"
+  },
 ]
