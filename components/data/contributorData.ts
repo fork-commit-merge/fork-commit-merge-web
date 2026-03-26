68c0aeb2639de63c55ee1fc10f47bdb2280738ae
GitHub Action <action@github.com>
2026-03-26 15:23:53 +0000

    Add NishantkumarABES to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7662,4 +7662,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/244779835?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "SumanMoharana24"
   },
+  {
+    id: "NishantkumarABES",
+    url: "https://github.com/NishantkumarABES",
+    avatar: "https://avatars.githubusercontent.com/u/94185893?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "NishantkumarABES"
+  },
 ]
