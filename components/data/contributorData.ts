4e601bb5e073faff513d660a581d7eb31358b2e6
GitHub Action <action@github.com>
2026-03-29 13:25:03 +0000

    Add iamabhijeet20 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7668,4 +7668,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/94185893?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "NishantkumarABES"
   },
+  {
+    id: "iamabhijeet20",
+    url: "https://github.com/iamabhijeet20",
+    avatar: "https://avatars.githubusercontent.com/u/43907897?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "iamabhijeet20"
+  },
 ]
