6ea5ce3f12b33c6726d452b4a74991cc66f0d683
GitHub Action <action@github.com>
2026-05-06 19:38:09 +0000

    Add Poonam-Biradar to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7848,4 +7848,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/90265074?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "thosewhoyoulove"
   },
+  {
+    id: "Poonam-Biradar",
+    url: "https://github.com/Poonam-Biradar",
+    avatar: "https://avatars.githubusercontent.com/u/149887192?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "Poonam-Biradar"
+  },
 ]
