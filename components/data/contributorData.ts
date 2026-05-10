c358ada52efa755fb07df4d59574ab87b337b8ea
GitHub Action <action@github.com>
2026-05-10 09:50:37 +0000

    Add madnessqw to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7854,4 +7854,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/149887192?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Poonam-Biradar"
   },
+  {
+    id: "madnessqw",
+    url: "https://github.com/madnessqw",
+    avatar: "https://avatars.githubusercontent.com/u/207336087?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "madnessqw"
+  },
 ]
