cda7a219a15c2cf9faa2bc1e1be5a6cbf19b82c0
GitHub Action <action@github.com>
2026-05-18 20:02:54 +0000

    Add zhihai888 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7902,4 +7902,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/183678257?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Aidenrmz"
   },
+  {
+    id: "zhihai888",
+    url: "https://github.com/zhihai888",
+    avatar: "https://avatars.githubusercontent.com/u/285636196?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "zhihai888"
+  },
 ]
