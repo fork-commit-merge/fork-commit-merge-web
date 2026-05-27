1338902abf2b204d4e63e3823ff03f859fc3f162
GitHub Action <action@github.com>
2026-05-27 06:38:46 +0000

    Add salkalero to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -7968,4 +7968,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/119715117?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Krow1224"
   },
+  {
+    id: "salkalero",
+    url: "https://github.com/salkalero",
+    avatar: "https://avatars.githubusercontent.com/u/48950749?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "salkalero"
+  },
 ]
