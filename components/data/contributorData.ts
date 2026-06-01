41a67c62a71869f09ce41266328b9cf7e1e1ccfd
GitHub Action <action@github.com>
2026-06-01 19:59:17 +0000

    Add Mositat7 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -8010,4 +8010,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/189255710?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "bruhjeshhh"
   },
+  {
+    id: "Mositat7",
+    url: "https://github.com/Mositat7",
+    avatar: "https://avatars.githubusercontent.com/u/173060745?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "Mositat7"
+  },
 ]
