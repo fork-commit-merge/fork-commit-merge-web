6995daac24130d90042e422bf74437ddc077bc70
GitHub Action <action@github.com>
2026-06-03 09:43:25 +0000

    Add veerlakshay to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -8016,4 +8016,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/173060745?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Mositat7"
   },
+  {
+    id: "veerlakshay",
+    url: "https://github.com/veerlakshay",
+    avatar: "https://avatars.githubusercontent.com/u/173218492?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "veerlakshay"
+  },
 ]
