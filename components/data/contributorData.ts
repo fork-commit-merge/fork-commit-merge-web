e8965997626a39022ae8a2adad1687b879181487
GitHub Action <action@github.com>
2026-06-13 19:02:28 +0000

    Add strangertomycode to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -8064,4 +8064,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/148376091?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "monushah108"
   },
+  {
+    id: "strangertomycode",
+    url: "https://github.com/strangertomycode",
+    avatar: "https://avatars.githubusercontent.com/u/114813900?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "strangertomycode"
+  },
 ]
