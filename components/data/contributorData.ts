1e6adc071ae1d9421c2e54337f58ac235d3b6f84
GitHub Action <action@github.com>
2026-06-20 16:34:37 +0000

    Add adela320 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -8100,4 +8100,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/248350285?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "priyanshu6m"
   },
+  {
+    id: "adela320",
+    url: "https://github.com/adela320",
+    avatar: "https://avatars.githubusercontent.com/u/72125200?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "adela320"
+  },
 ]
