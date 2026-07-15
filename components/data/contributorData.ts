5dd0889364b30af811ad1d181c4ff020df1b4f8f
GitHub Action <action@github.com>
2026-07-15 06:43:17 +0000

    Add krsnaSuraj to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -8226,4 +8226,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/274878645?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "serapeon"
   },
+  {
+    id: "krsnaSuraj",
+    url: "https://github.com/krsnaSuraj",
+    avatar: "https://avatars.githubusercontent.com/u/242935480?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "krsnaSuraj"
+  },
 ]
