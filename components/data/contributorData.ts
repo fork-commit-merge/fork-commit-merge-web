987cf4f040cf0daf42fd43b3d6f710181c04d865
GitHub Action <action@github.com>
2026-07-28 12:46:34 +0000

    Add zainabamjadx7 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -8310,4 +8310,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/167565054?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Tanvi-Kuwar"
   },
+  {
+    id: "zainabamjadx7",
+    url: "https://github.com/zainabamjadx7",
+    avatar: "https://avatars.githubusercontent.com/u/280486983?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "zainabamjadx7"
+  },
 ]
