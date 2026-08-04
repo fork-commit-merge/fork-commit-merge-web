39ae33d46c948d730d5eb633764a7c09257ac0a4
GitHub Action <action@github.com>
2026-08-04 06:52:31 +0000

    Add abdd8433 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -8328,4 +8328,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/114866607?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "TraptiSingh7300"
   },
+  {
+    id: "abdd8433",
+    url: "https://github.com/abdd8433",
+    avatar: "https://avatars.githubusercontent.com/u/224832252?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "abdd8433"
+  },
 ]
