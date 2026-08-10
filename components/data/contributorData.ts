00dd5075faa7b69903f5618f23a51229e1bbd137
GitHub Action <action@github.com>
2026-08-10 16:51:04 +0000

    Add SonikaSawant22 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -8346,4 +8346,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/76203871?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "only1isthereyatharth"
   },
+  {
+    id: "SonikaSawant22",
+    url: "https://github.com/SonikaSawant22",
+    avatar: "https://avatars.githubusercontent.com/u/197555101?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "SonikaSawant22"
+  },
 ]
