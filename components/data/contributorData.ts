095da43f5f607f7c5d38734524fdeb7ebfdc2052
GitHub Action <action@github.com>
2026-08-13 09:09:42 +0000

    Add elamathi3012 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -8382,4 +8382,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/185788424?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "DurgaTharshini26"
   },
+  {
+    id: "elamathi3012",
+    url: "https://github.com/elamathi3012",
+    avatar: "https://avatars.githubusercontent.com/u/199631332?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "elamathi3012"
+  },
 ]
