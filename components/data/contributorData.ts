7f3582d5ce251b1b71e54513a7c6e3147615f648
GitHub Action <action@github.com>
2026-08-17 05:10:10 +0000

    Add anchal125 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -8400,4 +8400,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/137482480?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "parvezpappu"
   },
+  {
+    id: "anchal125",
+    url: "https://github.com/anchal125",
+    avatar: "https://avatars.githubusercontent.com/u/98821108?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "anchal125"
+  },
 ]
