b9b74e796fd2d4a8c4e400b811736e40fc8b3fc0
GitHub Action <action@github.com>
2026-08-31 15:38:05 +0000

    Add yahiamohamed16 to contributors list

--- components/data/contributorData.ts
+++ components/data/contributorData.ts
@@ -8430,4 +8430,10 @@ export const contributorData = [
     avatar: "https://avatars.githubusercontent.com/u/205436333?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
     name: "Saket7002"
   },
+  {
+    id: "yahiamohamed16",
+    url: "https://github.com/yahiamohamed16",
+    avatar: "https://avatars.githubusercontent.com/u/221331818?v=4&h=300&w=300&fit=cover&mask=circle&maxage=7d",
+    name: "yahiamohamed16"
+  },
 ]
